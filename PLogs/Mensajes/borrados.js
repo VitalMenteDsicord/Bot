cec2ddcc0df3b65e6b550582a2243482e0acd8e6
Glitch (hello-express) <none>
2020-06-01 16:22:58 +0000

    🎳🐡 Checkpoint ./PLogs/Mensajes/borrados.js:6219301/22 ./watch.json:6219301/422

--- PLogs/Mensajes/borrados.js
+++ PLogs/Mensajes/borrados.js
@@ -12,17 +12,17 @@ module.exports.run = async (bot, message) => {
       let userID = logs.entries.first().executor.id;
       let userAvatar = logs.entries.first().executor.avatarURL();  
       
-    let Embed = new Discord.MessageEmbed()
-      .setColor(0xFF0202)
-      .setTitle("Mensaje eliminado")
-      .setThumbnail(userAvatar)
-      .addField("Contenido del mensaje", contenido)
-      .addField("🆔 Mensaje ID 🆔", messageid)
-      .addField("Autor del mensaje", `<@${message.author.id}> (ID: ${message.author.id})`)
-      .addField("Canal", message.channel)
-      .addField("Por", `<@${userID}> (ID: ${userID})`)
-      .setFooter(message.guild.name, message.guild.iconURL())
-    bot.channels.cache.get("716962193475633164").send(Embed);
+      let Embed = new Discord.MessageEmbed()
+        .setColor(0xFF0202)
+        .setTitle("Mensaje eliminado")
+        .setThumbnail(userAvatar)
+        .addField("Contenido del mensaje", contenido)
+        .addField("🆔 Mensaje ID 🆔", messageid)
+        .addField("Autor del mensaje", `<@${message.author.id}> (ID: ${message.author.id})`)
+        .addField("Canal", message.channel)
+        .addField("Por", `<@${userID}> (ID: ${userID})`)
+        .setFooter(message.guild.name, message.guild.iconURL())
+      bot.channels.cache.get("716962193475633164").send(Embed);
     });
   
 
