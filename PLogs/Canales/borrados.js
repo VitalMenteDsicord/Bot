47f271f0bde647d826c4e6aeffa6ff06856f7d33
Glitch (hello-express) <none>
2020-06-08 12:51:46 +0000

    📙🏞 Checkpoint ./PLogs/Canales/borrados.js:6219301/75 ./watch.json:6219301/422

--- PLogs/Canales/borrados.js
+++ PLogs/Canales/borrados.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, channel) => {
-  
   let canalid = channel.id;
   let canalname = channel.name;
   let tipo = channel.type;
