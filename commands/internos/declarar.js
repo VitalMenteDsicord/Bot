d18d41f21625ef763ad8801b4a6158bf31cf5f3d
Glitch (weazelnews) <none>
2020-08-20 00:42:29 +0000

    🐥🐫 Checkpoint ./commands/internos/error/error.js:6219301/1603 ./commands/internos/declarar.js:6219301/1159 ./config/config.json:6219301/72

--- commands/internos/declarar.js
+++ commands/internos/declarar.js
@@ -1,11 +1,15 @@
 const funciones = require("./functions/functions.js"); const errores = require("./error/error.js");
 /* LIBRRIAS */
-const Discord = require("discord.js"); const db = require('megadb');
+const Discord = require("discord.js"); const db = require('megadb'); const {readFileSync} = require("fs");
 /*DATA */
 const Dinero = new db.crearDB('dinero', 'Contabilidad');
 
 module.exports.run = async (bot, message) => {
     let prefix = bot.prefix;
+
+    let datos = readFileSync('./config/config.json');
+    let channel = JSON.parse(datos).CInternos.RDinero;
+
     let argumentos = message.content.split(",,");
     let DeclaradoAC = Dinero.tiene(`${message.guild.id}.declarado`) ? await Dinero.obtener(`${message.guild.id}.declarado`): 0;
 
@@ -16,7 +20,18 @@ module.exports.run = async (bot, message) => {
     let PrecioFa = Number(argumentos[2]);
     let NotasAD = argumentos[3];
     if(!ServicioOF ||  !PrecioCa || !PrecioFa || !NotasAD){let tipo = 1;;errores.run(bot, message, tipo);return null};
+    if(PrecioFa.includes("NaN")){let tipo = 2;;errores.run(bot, message, tipo);return null};
 
+    let embed = new Discord.MessageEmbed()
+        .setColor(0xFF2D00)
+        .setTitle("Registro de pagos")
+        .setThumbnail("https://media.discordapp.net/attachments/708083728123035736/714404437384364062/PinClipart.com_note-pad-clip-art_1706747.png?width=806&height=702")
+        .addField("Servicio Ofrecido", ServicioOF)
+        .addField("Precio catálogo", "$ "+PrecioCa)
+        .addField("Precio facturado", "$ "+PrecioFa)
+        .addField("Notas adicionales", NotasAD)
+        .addField("Dinero transferido a cúpula", "NO")
+    bot.channels.cache.get(channel).send({embed}).then(m =>{m.react('💰');}).catch();
 
 }
 
--- commands/internos/error/error.js
+++ commands/internos/error/error.js
@@ -10,10 +10,20 @@ module.exports.run = async (bot, message, tipo) => {
                 .setColor(0xFF0000)
                 .setTitle("ERROR")
                 .addField("Código de error", "0x0001")
-                
+                .addField("Estado","No has introducido todos los parámetros necesarios.")
+                .addField("Plantilla del comando", bot.prefix+"declarar Servicio Ofrecido ,, Precio catálogo ,, Precio facturado ,, Notas adicionales")
+                .setFooter("Un día sin sol es, ya sabes, de noche")
             message.author.send({embed}).catch();
         break;
-
+        case 2:
+            embed = new Discord.MessageEmbed()
+                .setColor(0xFF0000)
+                .setTitle("ERROR")
+                .addField("Código de error", "0x0002")
+                .addField("Estado","Recuerda que las cantidades de dinero se deben introducir sin puntos ni comas de por medio.")
+                .setFooter("Me hago responsable de lo que digo, no de lo que entiendas")
+            message.author.send({embed}).catch();
+        break;
 
     }
 
