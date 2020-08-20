08914d20c900f4fee4c45709e94540171004643f
Glitch (weazelnews) <none>
2020-08-20 03:24:10 +0000

    🍐👾 Checkpoint ./commands/internos/pago.js:6219301/322

--- commands/internos/pago.js
+++ commands/internos/pago.js
@@ -5,7 +5,16 @@ const Discord = require("discord.js");const generator = require('generate-passwo
 
 module.exports.run = async (bot, message) => {
     let prefix = bot.prefix;
+    let argumentos = message.content.split(",,");
 
+    let CantidadAP = argumentos[0].replace(prefix+"pago",'');
+    let Beneficiario = argumentos[1];
+    let Ncuenta = argumentos[2];
+    let codigo = generator.generate({length: 5, numbers: true});
+
+
+
+    
 };
 
 
