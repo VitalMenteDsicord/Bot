1f63e34fe7f6d2a382f85eb37c4f65aa9a66e3a5
Glitch (weazelnews) <none>
2020-08-21 14:53:28 +0000

    🍧🍎 Checkpoint ./commands/internos/TicketRepair.js:6219301/203 ./mega_databases/Contabilidad/dinero.json:6219301/28 ./config/config.json:6219301/226

--- commands/internos/TicketRepair.js
+++ commands/internos/TicketRepair.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js"); const db = require('megadb');
 
+const TicketUser = new db.crearDB('iduser', 'tickets');
+
 module.exports.run = async (bot, message) => {
 
-
+    let administrador = message.member.hasPermission('ADMINISTRATOR');
 
 }
+
+
+
+module.exports.help = {
+    name: "rtick"
+};
