40c00d4976ae4e3e72d74ab5232efb98e95cdc79
Glitch (weazelnews) <none>
2020-08-21 04:24:45 +0000

    💋🐪 Checkpoint ./commands/internos/Rmoney.js:6219301/1251

--- commands/internos/Rmoney.js
+++ commands/internos/Rmoney.js
@@ -5,21 +5,22 @@ const Discord = require("discord.js"); const db = require('megadb'); const {read
 const Dinero = new db.crearDB('dinero', 'Contabilidad');
 
 module.exports.run = async (bot, message) => {
-    let prefix = bot.prefix;
-
-    let datos = readFileSync('./config/config.json');
+    let servidor = message.guild;let datos = readFileSync('./config/config.json');
     let rolesADM = JSON.parse(datos).CInternos.Dinero.RMRoles;
 
-    for(let i = 0;i<ListaRolesP.length; i++){
-        let role = ListaRolesP[i];
-        let hasrole = servidor.members.cache.get(user.id)._roles.includes(role);
+    for(let i = 0;i<rolesADM.length; i++){
+        let role = rolesADM[i];
+        let hasrole = servidor.members.cache.get(message.author.id)._roles.includes(role);
         if(hasrole)
         {
-            try
-            {
-                bot.channels.cache.get(canalActual.id).delete();
-                TicketChannelA.eliminar(canalActual.id);
-            } catch(err){console.log(err)}
+            Dinero.establecer(`${message.guild.id}.declarado`, 0);
+            Dinero.establecer(`${message.guild.id}.trasferido`, 0);
+            let embed = new Discord.MessageEmbed()
+                .setColor(0x890202)
+                .setTitle("Dinero reiniciado")
+                .addField("Sin trasferir a cupula","$ "+0)
+                .addField("Transferidas a cupula","$ "+0)
+            message.channel.send({embed}).catch();
         }
     }
 
