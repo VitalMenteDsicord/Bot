165b7798522d81e68fa1190ece8735ecd8bd59e0
Glitch (lsma) <none>
2020-08-22 23:45:35 +0000

    🌷🐂 Checkpoint ./commands/internos/unidades.js:6219301/1009:21439783/1696

--- commands/internos/unidades.js
+++ commands/internos/unidades.js
@@ -3,46 +3,16 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message) => {
 
   
-  // Colores: https://htmlcolorcodes.com/es/
-  
-  /*
-    Plantilla completa EMBED:
-    
-    	.setColor('#0099ff')
-	    .setTitle('Some title')
-	    .setURL('https://discord.js.org/')
-	    .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	    .setDescription('Some description here')
-	    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	    .addFields(
-		    { name: 'Regular field title', value: 'Some value here' },
-		    { name: '\u200B', value: '\u200B' },
-		    { name: 'Inline field title', value: 'Some value here', inline: true },
-		    { name: 'Inline field title', value: 'Some value here', inline: true },
-	    )
-	    .addField('Inline field title', 'Some value here', true)
-	  .setImage('https://i.imgur.com/wSTFkRM.png')
-	  .setTimestamp()
-	  .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-    
-  */
-  
-  
     let embed = new Discord.MessageEmbed()
-        //.setAuthor('LSMA', 'https://i.imgur.com/LJClyPX.png', 'https://www.lu-rp.es/foro/thread-10448.html')
-        .setTitle("Soporte Vital Avanzado | UVI-1")
-        .addField("Soporte Vital Avanzado | UVI-1")
-        .setImage('https://i.imgur.com/pAdMTzk.png')
-        .setColor(0x037FDA)
- 
-  	    .setFooter('LSMA® DATA CENTER ', 'https://images.vexels.com/media/users/3/136810/isolated/preview/85a11ddd9eaf0e9870707a68d640a5f8-icono-de-burbuja-de-servidor-by-vexels.png');
+        .setTitle("Soporte Vital Avanzado")
+        .setDescription('Disponible.')
+        .setColor(0x00F221)
+        .setImage('https://i.imgur.com/3We2OWv.png')
     message.channel.send({embed});
 
-  
-  // `Puedo meter variables dentro ${message}`, 'Solo texto' , "Solo texto"
 };
 
 
 module.exports.help = {
-    name: "unidades" // Comando que tienes que poner en discord 
+    name: "unidades" 
 };
