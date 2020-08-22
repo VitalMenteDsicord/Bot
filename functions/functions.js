80743606b00d8fe5b930073b84b9e7561578891d
Glitch (weazelnews) <none>
2020-08-22 20:21:19 +0000

    🐺🐌 Checkpoint ./functions/functions.js:6219301/297 ./watch.json:6219301/992

--- functions/functions.js
+++ functions/functions.js
@@ -1,19 +1,16 @@
 const Discord = require('discord.js'); let { readdirSync } = require('fs'); 
 
 function precenseUpdate(bot) {
-    let datos = ["Weazel News 📰" , "¡Dando las verdaderas noticias!", "Leyendas Urbanas RP🎭"]
+    let datos = ["Los Santos Medical Assistance" , "¡Tu vida, es la nuestra!", "Leyendas Urbanas RP🎭"]
     setInterval(function() {
             bot.user.setActivity(datos[0], {type:'PLAYING'});
             setTimeout(function() {
                 bot.user.setActivity(datos[1], {type:'PLAYING'});
                 setTimeout(function() {
                     bot.user.setActivity(datos[2], {type:'PLAYING'});
-                    setTimeout(function() {
-                        bot.user.setActivity("Prefijo actual "+bot.prefix, {type:'PLAYING'});
-                    }, 5000)
                 }, 5000)
             }, 5000)
-    }, 20000)
+    }, 15000)
 }
 
 
