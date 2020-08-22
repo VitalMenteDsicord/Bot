4533ad5522a12334fc8d241852b47321493dda57
Glitch (lsma) <none>
2020-08-22 22:47:35 +0000

    👖🗃 Checkpoint ./commands/internos/cuadradotexto.js:6219301/3372:21439783/711

--- commands/internos/cuadradotexto.js
+++ commands/internos/cuadradotexto.js
@@ -31,15 +31,18 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
         //.setAuthor('LSMA', 'https://i.imgur.com/LJClyPX.png', 'https://www.lu-rp.es/foro/thread-10448.html')
         .setTitle("¡Bienvenid@ a nuestra aplicación!")
-        .setDescription("Empresa médica dedicada a emergencias sanitarias, traslados no urgentes, farmacología y psquiatría")
+        .setDescription("Empresa médica dedicada a emergencias sanitarias, traslados no urgentes, farmacología y psquiatría ")
         .setColor(0x037FDA)
-        .addField("Guía de uso de nuestra aplicación", 
-                   "- ¿Quieres saber más sobre nuestra empresa? <#737592540877946970> "+"\n"+
-                   "- ¿Quieres unirte al equipo? #unete-al-equipo"+"\n"+
-                   "- ? #unete-al-equipo"+"\n"+
-                   "- ¿Quieres unirte al equipo? #unete-al-equipo"+"\n"+
-                   "- ¿Quieres unirte al equipo? #unete-al-equipo"+"\n"
-                 );
+        .addField("Menú de navegación", "¡Accede directamente desde aquí al apartado que desees!")
+    
+        .addField("Nuestra empresa", "<#737592540877946970>" , true)
+        .addField("Unete a nosotros", "<#737736774218154094>" , true)
+        .addField("Farmacia", "<#737591727962980352>" , true)
+    
+        .addField("Seguro extrahospitalario", "<#737815244117901452>" , true)
+        .addField("Unidad Psiquiatría", "<#746086788610916493>" , true)
+        .addField("Ubicación", "<#737730544623812660>" , true)
+  
     message.channel.send({embed});
 
 };
