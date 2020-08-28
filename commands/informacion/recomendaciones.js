ac9452ce935e9318a5a680d8025afd9654b7f4e5
Glitch (lsma) <none>
2020-08-28 02:11:28 +0000

    🐠⛹ Checkpoint ./commands/informacion/localizacion.js:21439783/186 ./commands/informacion/recomendaciones.js:21439783/125

--- commands/informacion/recomendaciones.js
+++ commands/informacion/recomendaciones.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message) => {
 
         .setTitle("¡Recomiendanos!")
         .setDescription("¿Quieres felicitar a la empresa o algún miembro de la misma? Hazlo con la siguiente plantilla: \n \n Tipo de intervención: *Ejemplo: Accidente entre dos furgonetas en Little Seoul.* \n Día del suceso: *XX/XX/2020* \n ¿Que te ha parecido la atención recibida?: --")
-        .setImage('https://cdn.discordapp.com/attachments/742509588259012719/742513711272427591/Recomendacion.png')
+        .setImage('https://i.imgur.com/qmqkgHl.png')
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
