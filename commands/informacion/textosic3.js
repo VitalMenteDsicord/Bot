247683ac810c3e3bea95152e152f4eda2dc04434
Glitch (lsma) <none>
2020-08-29 17:57:21 +0000

    🚈🐆 Checkpoint ./commands/informacion/textosic3.js:21439783/118 ./commands/informacion/textoic4.js:21439783/2062

--- commands/informacion/textosic3.js
+++ commands/informacion/textosic3.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Primeros pasos en un aviso")
-        .setDescription("/me coge el equipamiento médico (botiquin y mochila de ataque) y se coloca los guantes de latex. \n \n /me examina al paciente, palpando su cuerpo en busca de hemorragias, hematomas y pertuberancias")
+        .setDescription("/me coge el equipamiento médico (botiquin y mochila de ataque) y se coloca los guantes de latex. \n \n /me examina al paciente, palpando su cuerpo en busca de hemorragias, hematomas y posibles fracturas \n \n /do ¿Que se podría observar en el paciente a simple vista? ")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
