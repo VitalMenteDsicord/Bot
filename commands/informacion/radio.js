66292e12dc854a5735b1739b7ee04d130138661d
Glitch (lsma) <none>
2020-08-30 21:17:37 +0000

    📅🎩 Checkpoint ./commands/informacion/radio.js:21439783/35

--- commands/informacion/radio.js
+++ commands/informacion/radio.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("RADIO TEMPORAL TS3")
-        .setDescription("LSMA.teamspeak.de")
+        .setDescription("178.33.161.79:9083")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
