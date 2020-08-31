65bfe4af0e783573b3947eb87edf331039e4d5a5
Glitch (lsma) <none>
2020-08-31 06:11:43 +0000

    ⛷💂 Checkpoint ./commands/informacion/psicologia.js:21439783/1494

---
+++ commands/informacion/psicologia.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message) => {
+  
+    let embed = new Discord.MessageEmbed()
+
+        .setTitle("Información de Psicología")
+        .setDescription("Estamos trabajando dia y noche para crear la unidad psicologica de LSMA.")
+        .setColor(0x037FDA) 
+    message.channel.send({embed});
+
+};
+
+
+module.exports.help = {
+    name: "psicologia"
+};
