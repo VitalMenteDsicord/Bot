f694e4afbe25656732b4d715aa1214ff6a459104
Glitch (lsma) <none>
2020-09-01 10:13:03 +0000

    🍃😺 Checkpoint ./commands/informacion/normativaactualizacion.js:21439783/90

--- commands/informacion/normativaactualizacion.js
+++ commands/informacion/normativaactualizacion.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Normativa Interna | Uso de comandos ")
-        .setDescription("Se añade a partir del día 01/09 de 2020, el punto de Uso de comandos. \n \n Se incluye normativa de los comandos: /d /deservicio /f y /r \n\n ((Normativa OOC))")
+        .setDescription("Se añade a partir del día 01/09 de 2020, el punto de Uso de comandos.")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
