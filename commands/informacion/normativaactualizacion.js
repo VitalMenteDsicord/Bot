7467f0e4a5cfe08be6e9d59b2ea55ee447fc5b1f
Glitch (lsma) <none>
2020-09-01 09:01:59 +0000

    🕌🗻 Checkpoint ./commands/informacion/normativaactualizacion.js:21439783/444

--- commands/informacion/normativaactualizacion.js
+++ commands/informacion/normativaactualizacion.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Normativa Interna | Modificación en punto 6")
-        .setDescription("Se modifica a partir del día 31/08 de 2020, el punto 6.1 y 6.2 del contrato laboral. \n \n A partir de este momento, se podrá pertenecer a otra empresa mientras no tenga relación con la sanidad y siempre con permiso expreso de la cúpula de LSMA. \n\n ((No se podrá pertenecer a una facción mientras se está en LSMA))")
+        .setTitle("Normativa Interna | Uso de comandos ")
+        .setDescription("Se añade a partir del día 01/09 de 2020, el punto de Uso de comandos. \n \n Se incluye normativa de los comandos: /d /deservicio /f y /r \n\n ((Normativa OOC))")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
