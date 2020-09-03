519b7baa12075c3fb38c05e9e509d81b91895a1f
Glitch (lsma) <none>
2020-09-03 07:22:44 +0000

    🏬👭 Checkpoint ./commands/informacion/aviso.js:21439783/437

--- commands/informacion/aviso.js
+++ commands/informacion/aviso.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Actualización 03/09/2020 LSMA®")
-        .setDescription("**Estas son las labores que se realizarán hoy: \n \n - Expulsión de miembros inactivos \n - Organización del evento de Picnic Comunitario \n - Selección de miembros para requerimiento en Evento.")
+        .setDescription("Estas son las labores que se realizarán hoy: \n \n - Expulsión de miembros inactivos \n - Organización del evento de Picnic Comunitario \n - Selección de miembros para requerimiento en Evento. \n - Nuevos textos en Textos IC \n")
         .setColor(0xDD0B0B) 
     message.channel.send({embed});
 
@@ -13,5 +13,5 @@ module.exports.run = async (bot, message) => {
 
 
 module.exports.help = {
-    name: "evento" 
+    name: "aviso"
 };
