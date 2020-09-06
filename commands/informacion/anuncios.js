f4f3cb8919b9acd87bccf67b63d0c0105c18c596
Glitch (lsma) <none>
2020-09-06 10:49:44 +0000

    🎲🐴 Checkpoint ./commands/informacion/anuncios.js:21439783/263

--- commands/informacion/anuncios.js
+++ commands/informacion/anuncios.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Plantilla anuncios")
-        .setDescription("/an Los Santos Medical Assistance | Empresa privada de Emergencias Sanitarias, Traslados no Urgentes, Farmacia Online. | +info LSMA APP \n \n /an ¿Tienes una empresa? Lleva un control de la salud de tus empleados, contrata ya nuestro Seguro Extrahospitalario | LSMA, accede a la APP. \n \n /an Los Santos Medical Assistance | ¡Te estamos buscando! ¿Quieres convertirte en técnico de emergencias sanitarias? | Unete hoy a LSMA.") 
+        .setDescription("/an LSMA® | Empresa privada de Emergencias Sanitarias, Traslados no Urgentes, Farmacia Online y Salud Mental | ¡Descarga la APP! \n \n /an ¿Tienes una empresa? Lleva un control de la salud de tus empleados, contrata ya nuestro Seguro Extrahospitalario | LSMA, accede a la APP. \n \n /an Los Santos Medical Assistance | ¡Te estamos buscando! ¿Quieres convertirte en técnico de emergencias sanitarias? | Unete hoy a LSMA.") 
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
