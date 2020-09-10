20acc5230d5528bed1d44d6b205fd968f7de7efc
Glitch (lsma) <none>
2020-09-10 11:39:25 +0000

    🌋🛣 Checkpoint ./commands/informacion/lsmadata pc.js:21439783/16

--- commands/informacion/lsmadata pc.js
+++ commands/informacion/lsmadata pc.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message) => {
 
         .setTitle("EQUIPAMIENTO MÉDICO")
         .setDescription("Detalles sobre nuestro equipamiento en UVI Movil: https://docs.google.com/document/d/1C9JTJgr55K6Z0QnEa3kueyDm6O5dattgMLbWxs9g4sM/edit?usp=sharing")
-        .setColor(0xF1EE16) 
+        .setColor(0x037FDA) 
     message.channel.send({embed});
 
 };
