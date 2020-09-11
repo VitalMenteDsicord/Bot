6e3dc39e14e19ca50929731fe76ce5cf6b0b464b
Glitch (lsma) <none>
2020-09-11 10:31:03 +0000

    🚥🍛 Checkpoint ./commands/informacion/infopsiquiatria.js:21439783/17

--- commands/informacion/infopsiquiatria.js
+++ commands/informacion/infopsiquiatria.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
         .setTitle("Información de Psiquiatría")
-        .setDescription("En LSMA hemos creado un nuevo servicio para adaptarse a los requerimientos de la ciudadanía, nuevo servicio de Psiquiatria: \n \n Podrás obtener ayuda con Trastornos de personalidad, daños causados por enfermedades mentales, trastornos sugestivos, síndromes, adicciones, problemas de comunicación con conyuge, rupturas amorosas... \n \n Tarifas: \n \n **1. Consulta básica → 500$** \n \n **2. Terapia → 1.300$ ** \n \n **3. Terapia Grupal → 1.700$** \n \n **4. Terapia de Pareja → 2.000 $** \n \n (Estas tarifas son por cada 15 minutos.)")
+        .setDescription("En LSMA hemos creado un nuevo servicio para adaptarse a los requerimientos de la ciudadanía, nuevo servicio de Psicología: \n \n Podrás obtener ayuda con Trastornos de personalidad, daños causados por enfermedades mentales, trastornos sugestivos, síndromes, adicciones, problemas de comunicación con conyuge, rupturas amorosas... \n \n Tarifas: \n \n **1. Consulta básica → 500$** \n \n **2. Terapia → 1.300$ ** \n \n **3. Terapia Grupal → 1.700$** \n \n **4. Terapia de Pareja → 2.000 $** \n \n (Estas tarifas son por cada 15 minutos.)")
         .setImage('https://i.imgur.com/JqwkIRt.png')
         .setColor(0x037FDA) 
     message.channel.send({embed});
