a504f9096c5ff777027dedbf397ca44db8e8bf38
Glitch (lsma) <none>
2020-09-17 10:42:29 +0000

    🎨👋 Checkpoint ./commands/informacion/dosclicks.js:21439783/709

---
+++ commands/informacion/dosclicks.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message) => {
+  
+    let embed = new Discord.MessageEmbed()
+
+        .setTitle("Aviso del uso de reacción")
+        .setDescription("/do Se vería como salen varios celadores del interior del hospital y trasladan al paciente la sala de urgencias, donde sería atendido.")
+        .setColor(0x037FDA) 
+    message.channel.send({embed});
+
+};
+
+
+module.exports.help = {
+    name: "reaccion" 
+};
