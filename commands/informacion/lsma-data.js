0e94ce2b1bceb0b1aacc80c19b687478e8a350e0
Glitch (lsma) <none>
2020-09-17 16:05:32 +0000

    😇😬 Checkpoint ./commands/informacion/lsma-data.js:21439783/143

--- commands/informacion/lsma-data.js
+++ commands/informacion/lsma-data.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message) => {
 
         .setTitle("LSMA DATA")
         .setDescription("Acceso a la aplicación LSMA DATA mobile \n \n ⌠**INFORME DE URGENCIAS**⌡ Rellena este informe en caso de acudir a un aviso, esto se incorporará a nuestra base de datos. https://forms.gle/xisRkRGZku9Dke1AA \n \n ⌠**PARTE DE INCIDENCIAS**⌡ Rellena este documento en caso de perdida de equipo, robo o accidente. https://forms.gle/BJ7kpuHeS4sJ9EnJA \n \n ⌠**TRASLADO SANITARIO NO URGENTE**⌡ Rellena este documento en caso de movilizar unidades para trasladar a un paciente de forma no urgente. https://forms.gle/cNGqSKxDNQTDRJUZ9")
-        .setImage('https://cdn.discordapp.com/attachments/737590348372770816/737774218208608316/app.png')
+        .setImage('https://i.imgur.com/8k7Jdgp.png')
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
