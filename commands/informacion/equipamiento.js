554237658e76f4c1b7f0b71b0618985c257e627c
Glitch (lsma) <none>
2020-09-22 21:03:33 +0000

    👷🌝 Checkpoint ./commands/informacion/equipamiento.js:21439783/246

--- commands/informacion/equipamiento.js
+++ commands/informacion/equipamiento.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("EQUIPAMIENTO MÉDICO UVI")
-        .setDescription("Detalles sobre nuestro equipamiento en UVI Movil: https://docs.google.com/document/d/1C9JTJgr55K6Z0QnEa3kueyDm6O5dattgMLbWxs9g4sM/edit?usp=sharing")
+        .setTitle("EQUIPAMIENTO MÉDICO VIR")
+        .setDescription("Detalles sobre nuestro equipamiento en Vehiculo de Intervención Rápida: https://docs.google.com/document/d/1HLoLfMZiwqtiqPDalFEcnzUMnKwDjwbN5GC8GH00CT0/edit?usp=sharing")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
