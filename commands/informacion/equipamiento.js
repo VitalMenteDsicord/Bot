185547626f4ab7ee3e3727d5ea71b8677bee175b
Glitch (lsma) <none>
2020-09-22 20:51:27 +0000

    👌🐪 Checkpoint ./commands/informacion/equipamiento.js:21439783/307

--- commands/informacion/equipamiento.js
+++ commands/informacion/equipamiento.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Guia médica avanzada")
-        .setDescription("a")
-        .setColor(0xC2E512) 
+        .setTitle("EQUIPAMIENTO MÉDICO UVI")
+        .setDescription("Detalles sobre nuestro equipamiento en UVI Movil: https://docs.google.com/document/d/1C9JTJgr55K6Z0QnEa3kueyDm6O5dattgMLbWxs9g4sM/edit?usp=sharing")
+        .setColor(0x037FDA) 
     message.channel.send({embed});
 
 };
 
 
 module.exports.help = {
-    name: "heridas" 
+    name: "equipamiento" 
 };
