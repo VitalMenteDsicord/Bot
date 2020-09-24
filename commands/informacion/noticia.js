91cafc311eaabae817ccdcc28d7a8b9a13419568
Glitch (lsma) <none>
2020-09-24 13:06:19 +0000

    🏉⛵️ Checkpoint ./commands/informacion/noticia.js:21439783/106

--- commands/informacion/noticia.js
+++ commands/informacion/noticia.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Cambios internos 24/09/2020")
-        .setDescription("- Se añade trello para la gestión interna de cúpula LSMA®. \n - Se incorporará un nuevo sistema de academia en el que se está trabajando. \n \n **¡Pronto tendréis más noticias!**")
+        .setDescription("- Se añade trello para la gestión interna de cúpula LSMA®. \n - Se incorporará un nuevo sistema de academia en el que se está trabajando. \n - Nuevo aparcamiento privado, se creará un canal para tener organización dentro de él. \n \n **¡Pronto tendréis más noticias!**")
         .setColor(0xDB0F0F) 
     message.channel.send({embed});
 
