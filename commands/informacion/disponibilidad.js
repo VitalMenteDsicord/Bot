7552b4188bcafa14189f6a3a9b735cb551d467ac
Glitch (lsma) <none>
2020-09-29 16:15:17 +0000

    🙈🚄 Checkpoint ./commands/informacion/disponibilidad.js:21439783/4

--- commands/informacion/disponibilidad.js
+++ commands/informacion/disponibilidad.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Horario de atención (28/09)")
+        .setTitle("Horario de atención (29/09)")
         .setDescription("Director LSMA® Jhon Wood. \n \n Horario disponible: **23:00** a **00:00** (Hora peninsular española) \n Correo électronico habilitado: contacto@lsma.es (Peticiones formales)")
         .setColor(0x037FDA) 
     message.channel.send({embed});
