97114ed2f383766112f82d3545b1290b7d3b85d7
Glitch (lsma-project) <none>
2020-10-11 22:39:33 +0000

    🎩🕹 Checkpoint ./commands/informacion/noticia.js:21439783/944

--- commands/informacion/noticia.js
+++ commands/informacion/noticia.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Cambios internos 07/10/2020")
-        .setDescription(" \n - Dentro de la facción habrá especialistas médicos, que podrán realizar operaciones dentro del Hospital y recibirán un gran payday. \n - Comisiones por tu desempeño dentro del Hospital. \n - Creación de nuevos roles, ya que tenemos que transportar al herido dentro del hospital además de rolear con él dentro. \n - Semana de reclutamiento. Puesto de RRHH y campañas públicitarias.")
+        .setTitle("Actualización 12/10/2020")
+        .setDescription("- El canal de información de academia se utilizará para cuando se realicen clases grupales, por ese medio se informara de día y hora. \n - En el canal de material didáctico se almacenará información de códigos radiales, contenido de equipamiento, guía de procedimientos, clases y normativa. \n - En el canal de solicitud podréis pedir una clase personalizada si no pudises asistir a alguna. \n - Para que los aspirantes puedan patrullar, deberán tener la clase nº1 completada. Para subir de rango deberán hacer un examen.")
         .setColor(0xDB0F0F) 
     message.channel.send({embed});
 
