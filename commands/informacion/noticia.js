b2118313ef99d9716975cc6b913cabfef4c978e0
Glitch (lsma-project) <none>
2020-10-11 22:28:17 +0000

    🎖🌏 Checkpoint ./commands/informacion/noticia.js:21439783/77

--- commands/informacion/noticia.js
+++ commands/informacion/noticia.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Cambios internos 07/10/2020")
-        .setDescription("- El hospital de Mount Zonah, pasa a ser el de principal uso para la facción. \n - Dentro de la facción habrá especialistas médicos, que podrán realizar operaciones dentro del Hospital y recibirán un gran payday. \n - Comisiones por tu desempeño dentro del Hospital. \n - Creación de nuevos roles, ya que tenemos que transportar al herido dentro del hospital además de rolear con él dentro. \n - Semana de reclutamiento. Puesto de RRHH y campañas públicitarias.")
+        .setDescription(" \n - Dentro de la facción habrá especialistas médicos, que podrán realizar operaciones dentro del Hospital y recibirán un gran payday. \n - Comisiones por tu desempeño dentro del Hospital. \n - Creación de nuevos roles, ya que tenemos que transportar al herido dentro del hospital además de rolear con él dentro. \n - Semana de reclutamiento. Puesto de RRHH y campañas públicitarias.")
         .setColor(0xDB0F0F) 
     message.channel.send({embed});
 
