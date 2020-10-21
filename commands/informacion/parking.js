63561af51400dc2e8a06abef6e48d9894505f176
Glitch (lsma-project) <none>
2020-10-21 21:42:38 +0000

    🌺👟 Checkpoint ./commands/informacion/parking.js:21439783/454

--- commands/informacion/parking.js
+++ commands/informacion/parking.js
@@ -5,9 +5,10 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Parking subterraneo | Abierto las 24h.")
-        .setDescription("Funcionamiento: Cada persona tiene 2 plazas de aparcamiento en este parking para sus vehículos, unicamente se podrá usar para dejar el vehiculo aparcado una vez empezamos el servicio. \n \n En este canal se deberá indicar **matricula, modelo y dueño del mismo.** Si tu vehículo no está en esta lista se procederá a retirar.")
+        .setDescription("Funcionamiento: Cada persona tiene 1 plaza de aparcamiento en este parking para su vehículo, unicamente se podrá usar para dejar el vehiculo aparcado una vez empezamos el servicio. \n (¡Puedes pedir una plaza adicional sin coste!) \n En este canal se deberá indicar **matricula, modelo y dueño del mismo.** Si tu vehículo no está en esta lista se procederá a retirar.")
         .setColor(0xD4D4D4) 
-        .setImage('https://i.imgur.com/1YX2fUa.png')
+        .setImage('https://i.imgur.com/zTVcAhu.png')
+        .setURL('')
     message.channel.send({embed});
 
 };
