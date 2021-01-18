5cb52234ad1d18aa7ba2de5ed8440949fa442ae2
Glitch (lsma-project) <none>
2021-01-18 02:53:38 +0000

    🔑🏦 Checkpoint ./commands/informacion/psicologia.js:27518021/43 ./commands/informacion/Tinfo.js:27518021/500

--- commands/informacion/Tinfo.js
+++ commands/informacion/Tinfo.js
@@ -8,22 +8,21 @@ module.exports.run = async (bot, message) => {
         .setTitle("Asistencia de Emergencias 24H.")
         .setColor(0x037FDA)
         .setDescription("¿Necesitas solicitar nuestra asistencia? Pulsa aquí📩 \n (Tiempo estimado de espera: De 2-5 minutos.)")
-        .setFooter('LSMA® DATA CENTER', 'https://images.vexels.com/media/users/3/136810/isolated/preview/85a11ddd9eaf0e9870707a68d640a5f8-icono-de-burbuja-de-servidor-by-vexels.png')
+        .setFooter('LSMA® DATA CENTER', )
     message.channel.send({embed}).then(m => m.react("📩")).catch();
      
     */
   
   
       let embed = new Discord.MessageEmbed()
-        .setTitle("Departamento de Psicología")
-        .setColor(0x037FDA)
-        .setImage("https://i.imgur.com/kIn26A8.png")
-        .setDescription("¡Habla con nuestra experta de la unidad de psicología! \n (Tiempo estimado de espera: De 2-10 minutos.)")
-        .setFooter('LSMA® DATA CENTER', 'https://images.vexels.com/media/users/3/136810/isolated/preview/85a11ddd9eaf0e9870707a68d640a5f8-icono-de-burbuja-de-servidor-by-vexels.png')
+        .setTitle("Departamento de Salud Mental")
+        .setColor(0x037FDA)    
+        .setDescription("¡Habla con uno de nuestros expertos en el Departamento de Salud Mental \n (Tiempo estimado de espera: De 2-10 minutos.)")
+        .setFooter('LSMA® DATA CENTER', )
     message.channel.send({embed}).then(m => m.react("📩")).catch();
 };
 
 
 module.exports.help = {
-    name: "Tinfo"
+    name: "saludm"
 };
