4341f9568bfaa04dc34dfb8499109aa86dd37af4
Glitch (lsma-project) <none>
2021-01-18 02:06:34 +0000

    🎑💑 Checkpoint ./commands/informacion/precios.js:27518021/29

--- commands/informacion/precios.js
+++ commands/informacion/precios.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Precios LSMA®")
-        .setDescription("Estos son los precios para eventos: \n \n - **UVI:** $5000 /hora \n \n - **VIR:** $7000  /hora \n \n - Hospital de Campaña $5000 /hora \n \n - Unidad Aerea $7500 /hora \n \n - Paramedico que asista $2500 /hora \n \n Se debera pedir los servicios 72 horas con antipacion.  ")
-        .setColor(0xDD0B0B) 
+        .setDescription("Estos son los precios para eventos: \n \n - **UVI:** $5000 /hora \n \n - **VIR:** $7000  /hora \n \n - **Hospital de Campaña:** $5000 /hora \n \n - **Unidad Aerea:** $7500 /hora \n \n - **Paramedico que asista:** $2500 /hora \n \n Se debera pedir los servicios 72 horas con antipacion.  ")
+        .setColor(0x15B4BE) 
     message.channel.send({embed});
 
 };
