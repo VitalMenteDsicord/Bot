f30ae47537a26d3bfe24afc3a00c97b71e8e0368
Glitch (lsma-project) <none>
2021-01-18 02:41:44 +0000

    🌨🚄 Checkpoint ./commands/informacion/parking.js:27518021/12 ./commands/informacion/seguroempresa.js:27518021/113 ./commands/informacion/seguro.js:27518021/27 ./commands/informacion/eventos.js:27518021/12

--- commands/informacion/eventos.js
+++ commands/informacion/eventos.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message) => {
 
         .setTitle("(Recordatorio) Evento Police Department, Viernes 4")
         .setDescription("**Ubicación:** Campo de Golf, Rockford Hills. \n **Hora del evento:** 22:00 (Hora peninsular española) \n **¿Que realizaremos alli?** Haremos un pequeño curso de primeros auxilios y mostraremos nuestras unidades con detalle. \n \n Recordamos que todos los que acudan tendrán un plus de 10.000$ y que se deberá entrar con anticipación para la preparación.")
-        .setColor(0x38BFB9) 
+        .setColor(0x037FDA) 
         .setImage('https://i.imgur.com/6acg7sv.png')
     message.channel.send({embed});
 
--- commands/informacion/parking.js
+++ commands/informacion/parking.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message) => {
 
         .setTitle("Parking subterraneo | Abierto las 24h.")
         .setDescription("Funcionamiento: Cada persona tiene 1 plaza de aparcamiento en este parking para su vehículo. \n (¡Puedes pedir una plaza adicional sin coste!) \n En este canal se deberá indicar **matricula, modelo y dueño del mismo.** Si tu vehículo no está en esta lista se procederá a retirar.")
-        .setColor(0x15B4BE) 
+        .setColor(0x037FDA) 
         .setImage('https://i.imgur.com/zTVcAhu.png')
         .setURL('https://docs.google.com/spreadsheets/d/1DSCV-Ol9Yd3Wi1WDV_kMmzS762oh_xpWVeV2qb8yXvk/edit?usp=sharing')
     message.channel.send({embed});
