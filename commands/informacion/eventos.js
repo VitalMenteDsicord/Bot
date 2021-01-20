6bd7824d1038379b8687af9aed9333cde8c92dfa
Glitch (lsma-project) <none>
2021-01-20 19:08:30 +0000

    🐺👻 Checkpoint ./commands/informacion/eventos.js:27518021/303

--- commands/informacion/eventos.js
+++ commands/informacion/eventos.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("(Recordatorio) Evento Police Department, Viernes 4")
+        .setTitle("(Nuevo Evento) Gymnasium - Nº servicio: 0007 Viernes 22/01/2020")
         .setDescription("**Ubicación:** Campo de Golf, Rockford Hills. \n **Hora del evento:** 22:00 (Hora peninsular española) \n **¿Que realizaremos alli?** Haremos un pequeño curso de primeros auxilios y mostraremos nuestras unidades con detalle. \n \n Recordamos que todos los que acudan tendrán un plus de 10.000$ y que se deberá entrar con anticipación para la preparación.")
         .setColor(0x38BFB9) 
         .setImage('https://i.imgur.com/6acg7sv.png')
