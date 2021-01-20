e7ea406dde36941b9c1f9281094ae93444ffe8ae
Glitch (lsma-project) <none>
2021-01-20 19:54:39 +0000

    🌛🗼 Checkpoint ./commands/informacion/Tinfo.js:27518021/122 ./commands/informacion/disponibilidaddg.js:27518021/1080

---
+++ commands/informacion/disponibilidaddg.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message) => {
+  
+    let embed = new Discord.MessageEmbed()
+
+        .setTitle("Horario de atención"
+        .setDescription("CCO Douglas Gibson LSMA® \n \n Horario disponible: **23:00** a **00:00** (Hora peninsular española) \n Correo électronico habilitado: contacto@lsma.es (Peticiones formales)")MA® Jhon Wood. \n \n Horario disponible: **23:00** a **00:00** (Hora peninsular española) \n Correo électronico habilitado: contacto@lsma.es (Peticiones formales)")
+        .setColor(0x037FDA) 
+    message.channel.send({embed});
+
+};
+
+
+module.exports.help = {
+    name: "douglas"
+};
