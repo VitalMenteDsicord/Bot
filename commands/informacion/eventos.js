18d7b3c5d940a2ef0711a9a9d6ebcac27f9f5c11
Glitch (lsma-project) <none>
2021-01-20 19:42:43 +0000

    📚🚉 Checkpoint ./commands/informacion/eventos.js:27518021/618

--- commands/informacion/eventos.js
+++ commands/informacion/eventos.js
@@ -4,10 +4,10 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("(Nuevo Evento) Gymnasium - Nº servicio: 0007 Viernes 22/01/2020")
-        .setDescription("**Ubicación:** Campo de Golf, Rockford Hills. \n **Hora del evento:** 22:00 (Hora peninsular española) \n **Hora de preparacion:** 21:30  \n                               lios y mostraremos nuestras unidades con detalle. \n \n Recordamos que todos los que acudan tendrán un plus de 10.000$ y que se deberá entrar con anticipación para la preparación.")
+        .setTitle("(Nuevo Evento) Gymnasium - Nº servicio: 0007 Viernes 22/01/2020") 
+        .setDescription("**Ubicación:** Campo de Golf, Rockford Hills. \n **Hora del evento:** 22:00 (Hora peninsular española) \n **Hora de preparacion:** 21:30  \n **Hora de finalizacion del evento:** 23:00 \n \n **Ubicacion:** Vienwood Hills \n \n **Unidades solicitadas:** 1 UVI y 2 Paramedicos")
         .setColor(0x38BFB9) 
-        .setImage('https://i.imgur.com/6acg7sv.png')
+        .setImage('https://cdn.discordapp.com/attachments/801488850991841311/801494455663722506/image0.png')
     message.channel.send({embed});
 
 };
