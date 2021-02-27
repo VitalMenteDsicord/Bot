7901e226c32b728f5d9a2974153e61d6b607b691
Glitch (lsma-project) <none>
2021-02-27 18:43:21 +0000

    📠🐆 Checkpoint ./commands/informacion/Tinfo.js:21439783/2 ./commands/informacion/valeiro.js:21439783/352

--- commands/informacion/Tinfo.js
+++ commands/informacion/Tinfo.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message) => {
         .setTitle("Departamento de Salud Mental")
         .setColor(0x037FDA)
         .setImage("")
-        .setDescription("¡Habla con nuestra experta de la unidad del Departamento de Salud Mental! \n (Tiempo estimado de espera: De 2-10 minutos.)")
+        .setDescription("¡Habla con nuestra experto de la unidad del Departamento de Salud Mental! \n (Tiempo estimado de espera: De 2-10 minutos.)")
         .setFooter('LSMA® DATA CENTER', 'https://images.vexels.com/media/users/3/136810/isolated/preview/85a11ddd9eaf0e9870707a68d640a5f8-icono-de-burbuja-de-servidor-by-vexels.png')
     message.channel.send({embed}).then(m => m.react("📩")).catch();
 };
--- commands/informacion/valeiro.js
+++ commands/informacion/valeiro.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Ayden McDowell")
-        .setDescription("Hola, que tal, me presento mi nombre es Ayden McDowell ....  soy **lider del Departamento del Sheriff** y todos los demas me comen los huevos, ademas pertenezco a **Major Crimes Bureau Head**, cambio.")
+        .setDescription("¿Quieres pertenecer a alguna de nuestras divisiones? Reacciona al emoticono correspondiente y te contactaremos desde asuntos internos.")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
