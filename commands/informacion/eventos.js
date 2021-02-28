54348573d9e1189ee2a0480a5a578cf184b2b683
Glitch (lsma-project) <none>
2021-02-28 20:38:23 +0000

    🔮🐩 Checkpoint ./commands/informacion/eventos.js:21439783/1228

--- commands/informacion/eventos.js
+++ commands/informacion/eventos.js
@@ -4,15 +4,14 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Ups, ¡Estamos en proceso!") 
-        .setDescription("**Ubicación:** Campo de Golf, Rockford Hills. \n **Hora del evento:** 22:00 (Hora peninsular española) \n **Hora de preparacion:** 21:30  \n **Hora de finalizacion del evento:** 23:00 \n \n **Ubicacion:** Vienwood Hills \n \n **Unidades solicitadas:** 1 UVI y 2 Paramedicos")
-        .setColor(0x38BFB9) 
-        .setImage('https://cdn.discordapp.com/attachments/801488850991841311/801494455663722506/image0.png')
+        .setTitle("Ups, parece que aquí no hay nada que mostrar.") 
+        .setDescription("En nada aquí apareceran los miembros de nuestro departamento, somos personas y a veces también tardones. \n \n ¡Te pedimos paciencia usuario!")
+        .setColor(0xF8281B) 
     message.channel.send({embed});
 
 };
 
 
 module.exports.help = {
-    name: "evento" 
+    name: "evento2" 
 };
