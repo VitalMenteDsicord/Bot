2451cf70515d43d9e68fa5416e9c901c1233e949
Glitch (lsma-project) <none>
2021-02-28 19:07:31 +0000

    🚈🏄 Checkpoint ./commands/informacion/localizacion.js:21439783/89

--- commands/informacion/localizacion.js
+++ commands/informacion/localizacion.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Los Santos Medical Assistance | Localización")
+        .setTitle("Pillbox Hills Medical Center | Localización")
         .setColor(0x037FDA)
         .setDescription("Nos encontramos justo en frente de LifeInvader, en Rockford Hills. ¿Tienes alguna duda? mandanos un correo a **info@lsma.es**")
         .setImage("https://i.imgur.com/nrIN7ez.jpg")
