9c7890025feb414d0612b873406246faa9bcf2b3
Glitch (lsma-project) <none>
2021-02-28 20:27:17 +0000

    🎭👋 Checkpoint ./commands/informacion/infopsiquiatria.js:21439783/911 ./commands/informacion/eventos.js:21439783/90

--- commands/informacion/eventos.js
+++ commands/informacion/eventos.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("(Nuevo Evento) Gymnasium - Nº servicio: 0007 Viernes 22/01/2020") 
+        .setTitle("Ups, ¡Estamos en proceso!") 
         .setDescription("**Ubicación:** Campo de Golf, Rockford Hills. \n **Hora del evento:** 22:00 (Hora peninsular española) \n **Hora de preparacion:** 21:30  \n **Hora de finalizacion del evento:** 23:00 \n \n **Ubicacion:** Vienwood Hills \n \n **Unidades solicitadas:** 1 UVI y 2 Paramedicos")
         .setColor(0x38BFB9) 
         .setImage('https://cdn.discordapp.com/attachments/801488850991841311/801494455663722506/image0.png')
--- commands/informacion/infopsiquiatria.js
+++ commands/informacion/infopsiquiatria.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
         .setTitle("Servicio ofrecido por LSMA®")
-        .setDescription("En LSMA hemos creado un nuevo departamento médico para la atención de todos los ciudadanos en Hospital de Pillbox Hills.")
-        .setImage('https://i.imgur.com/6kfkzrs.png')
+        .setDescription("En LSMA hemos creado un nuevo departamento médico para la atención de todos los ciudadanos en Hospital de Pillbox Hills. \n \n **Realizamos los siguientes servicios dentro del hospital:** \n \n - Operaciones quirurgicas. \n \n - Consultas médicas generales. \n \n - Atención de Urgencia. \n \n - TAC y pruebas diagnósticas. \n \n - Atención psicologica individual, pareja y en grupo. \n \n - Psicotécnicos para Caza \n \n - Consulta especializada para atención especial. \n \n - Parto y atención al embarazo. \n \n **Para más información contacta con nosotros en <#815651832515919902>** ")
+        .setImage('https://i.imgur.com/OHUkdOW.png')
         .setColor(0xF8281B) 
     message.channel.send({embed});
 
