308944405bf30af9792c4f71c95da56ed2154838
Glitch (lsma-project) <none>
2021-03-01 02:58:35 +0000

    🍤🛋 Checkpoint ./commands/informacion/textosic2.js:21439783/351 ./commands/informacion/textosic3.js:21439783/1044

--- commands/informacion/textosic3.js
+++ commands/informacion/textosic3.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Portal de citas médicas")
-        .setDescription("Accede al portal de LSMA para indicar una nueva fecha para consulta o operación. Recuerda marcar la tarea una vez realizada. https://cutt.ly/llKQ4Ak")
+        .setTitle("Procedimiento - Alta de Hospital")
+        .setDescription("/do Llega al box, un celador junto a varios papeles donde incluirian detalles sobre el ingreso del paciente. \n \n /do En estos, aparecerían datos del paciente y actos realizados en el hospital. Debajo del todo, una firma del médico y LSMA. \n \n /do Le harian entrega de los respectivos documentos al paciente. \n \n **(En caso de no rolear el acompañar al sujeto hacia la recepción)** /do Tras esto, le acompañarian hacia la recepción mientras se observa a varias personas aplaudir por la recuperación. ")
         .setColor(0xF8281B) 
     message.channel.send({embed});
 
@@ -13,5 +13,5 @@ module.exports.run = async (bot, message) => {
 
 
 module.exports.help = {
-    name: "citamedica" 
+    name: "citamedica22" 
 };
