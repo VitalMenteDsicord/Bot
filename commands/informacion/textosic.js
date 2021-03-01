51ee34e23cabbfe0a3b028125a1a5bedf603fb29
Glitch (lsma-project) <none>
2021-03-01 01:24:59 +0000

    🚝🌮 Checkpoint ./commands/informacion/textosic.js:21439783/552

--- commands/informacion/textosic.js
+++ commands/informacion/textosic.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Texto IC - Alta Voluntaria")
-        .setDescription("/do Se vería como en el teléfono aparece el apartado de Altas Voluntarias de LSMA. Aparecerían varios detalles como el cese de[...] \n \n /do [...] responsabilidades de LSMA una vez firmado este documento. Abajo aparecerían dos casillas: NOMBRE, FIRMA.")
+        .setTitle("Texto IC - Procedimientos")
+        .setDescription("/do Un celador llevaría al paciente hacia el BOX correspondiente. (Aquel donde acude el médico) \n \n /do Le realizarian toma de tensión, ")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
