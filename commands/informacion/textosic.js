84b01d33a186ddc1d20d1585f8a6897740cbf240
Glitch (lsma-project) <none>
2021-03-01 01:37:08 +0000

    🏮🏣 Checkpoint ./commands/informacion/textosic.js:21439783/222

--- commands/informacion/textosic.js
+++ commands/informacion/textosic.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Texto IC - Procedimientos")
-        .setDescription("/do Un celador llevaría al paciente hacia el BOX correspondiente. (Aquel donde acude el médico) \n \n /do Le realizarian toma de tensión, ")
+        .setDescription("/do Un celador llevaría al paciente hacia el BOX correspondiente. (Aquel donde acude el médico) \n \n /do Le realizarian toma de tensión, oxigeno en sangre y pulsaciones al paciente. \n \n /do ¿El paciente tendría algún parametro fuera de lo común? Si es así, ¿Cual? \n \n /do Proceden a la apertura de una via")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
