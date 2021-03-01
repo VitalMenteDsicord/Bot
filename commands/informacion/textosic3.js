0f21fca6ac90586de2d9d75d7b2de8ecf168eca7
Glitch (lsma-project) <none>
2021-03-01 02:12:22 +0000

    🌜👡 Checkpoint ./commands/informacion/textosic3.js:21439783/994

--- commands/informacion/textosic3.js
+++ commands/informacion/textosic3.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Texto IC - Primeros pasos en un aviso")
-        .setDescription("/me coge el equipamiento médico (botiquin y mochila de ataque) y se coloca los guantes de latex. \n \n /me examina al paciente, palpando su cuerpo en busca de hemorragias, hematomas y posibles fracturas \n \n /do ¿Que se podría observar en el paciente a simple vista? ")
-        .setColor(0x037FDA) 
+        .setTitle("Portal de citas médicas")
+        .setDescription("https://trello.com/invite/b/L3M9ETOF/ad429b4f68411c32e374f25347c391b6/citas-m%C3%A9dicas")
+        .setColor(0xF8281B) 
     message.channel.send({embed});
 
 };
 
 
 module.exports.help = {
-    name: "textoic3" 
+    name: "citamedica" 
 };
