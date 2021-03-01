9195100ca1d55edaf423022c7002013999899df5
Glitch (lsma-project) <none>
2021-03-01 02:35:30 +0000

    🐸👔 Checkpoint ./commands/informacion/textosic.js:21439783/167 ./commands/informacion/textosic2.js:21439783/2046

--- commands/informacion/textosic.js
+++ commands/informacion/textosic.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Texto IC - Procedimientos")
-        .setDescription("/do Un celador llevaría al paciente hacia el BOX correspondiente. (Aquel donde acude el médico) \n \n /do Le realizarian toma de tensión, oxigeno en sangre y pulsaciones al paciente. \n \n /do ¿El paciente tendría algún parametro fuera de lo común? Si es así, ¿Cual? \n \n /do Proceden a la apertura de una via")
-        .setColor(0x037FDA) 
+        .setTitle("Texto IC - Procedimientos 1")
+        .setDescription("/do Un celador llevaría al paciente hacia el BOX correspondiente. (Aquel donde acude el médico) \n \n /do Le realizarian toma de tensión, oxigeno en sangre y pulsaciones al paciente. \n \n /do ¿El paciente tendría algún parametro fuera de lo común? Si es así, ¿Cual?")
+        .setColor(0xF8281B) 
     message.channel.send({embed});
 
 };
 
 
 module.exports.help = {
-    name: "textoic" 
+    name: "textoicc" 
 };
--- commands/informacion/textosic2.js
+++ commands/informacion/textosic2.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Texto IC - Llegada al hospital")
-        .setDescription("/do Se vería como salen varios celadores del interior del hospital y trasladan al paciente la sala de urgencias, donde sería atendido.")
-        .setColor(0x037FDA) 
+        .setTitle("Procedimiento - Apertura de una via (En camilla)")
+        .setDescription("/me coge del bolsillo una pequeña cinta e intenta colocarsela al paciente en el brazo. \n \n /do Tras esto, ¿Las venas de su brazo serían visibles? \n \n /me coge un cateter, y procede a su colocación con sumo cuidado, tras esto lo enchufa al compresor.")
+        .setColor(0xF8281B) 
     message.channel.send({embed});
 
 };
 
 
 module.exports.help = {
-    name: "textoic2" 
+    name: "textoicc2" 
 };
