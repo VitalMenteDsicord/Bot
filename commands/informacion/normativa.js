f77238312e36774d5fd867fa8494a986efd2ced1
Glitch (lsma-project) <none>
2021-03-02 02:34:07 +0000

    👩🎈 Checkpoint ./commands/informacion/normativa.js:21439783/1343

--- commands/informacion/normativa.js
+++ commands/informacion/normativa.js
@@ -4,14 +4,16 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Normativa Interna | Los Santos Medical Assistance")
-        .setDescription("https://docs.google.com/document/d/1sRtScXMiK1OpnAELGTh_-GWC1NvGuk5bYzV_6rrKZg4/edit?usp=sharing \n \n Todos los cambios en la normativa serán actualizados mediante este apartado.")
-        .setColor(0x037FDA) 
+        .setTitle("Autorizados por LSMA®")
+        .setDescription("¡Hola! Se pone a disposición, esta lista de nuestros miembros del departamento médico autorizados a permanecer en el hospital y actuar como tal. \n \n **Director Médico** Jhon Wood \n \n **Director dept. Urgencias** Gustavo Cuevas \n \n **Dept. Salud Mental** Venancio Gutierrez \n \n **Enfermero** Marc Pinto \n \n **Auxiliar de enfermeria** Octavio Pando \n \n **Celador** Arturo Fernandez \n \n **Celador** Luke Brow \n \n **Celador** Mauro Suárez")
+        .setColor(0xF8281B) 
+        .setImage('https://i.imgur.com/CgXE8Vy.png')
+        .setFooter('Pillbox® Data Center ', 'https://i.imgur.com/lCJUNKB.png')
     message.channel.send({embed});
 
 };
 
 
 module.exports.help = {
-    name: "normativa" 
+    name: "autorizados" 
 };
