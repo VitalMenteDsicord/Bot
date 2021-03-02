8081d6baf026738d641307cfa5ecce20cc6e5c4f
Glitch (lsma-project) <none>
2021-03-02 21:05:46 +0000

    🎤🏈 Checkpoint ./commands/informacion/academia.js:21439783/532

--- commands/informacion/academia.js
+++ commands/informacion/academia.js
@@ -4,9 +4,10 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Uniforme médico")
-        .setDescription("¡Este es el)
+        .setTitle("Uniforme médico reglamentario")
+        .setDescription("¡Este es el uniforme que debes portar si estas en el hospital! \n \n **Lista de tiendas:** \n Mission Row \n Sub-Urban \n \n **Lista de ropa:** \n \n **Solicitar:** \n /reportar Necesito que me coloquéis el siguiente ropat: /ropat 7 126 0 ID")
         .setColor(0xF8281B) 
+        .setImage('https://i.imgur.com/Phin9KK.png')
     message.channel.send({embed});
 
 };
