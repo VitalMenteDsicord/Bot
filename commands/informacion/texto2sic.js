454d59598a719394798550a9f5b6b6b516b9cd3b
Glitch (lsma-project) <none>
2021-03-03 03:03:57 +0000

    🏪🛀 Checkpoint ./commands/informacion/textosic3.js:21439783/1679 ./commands/informacion/texto2sic.js:21439783/470

--- commands/informacion/texto2sic.js
+++ commands/informacion/texto2sic.js
@@ -4,9 +4,9 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Procedimiento - Retirar sangre y analítica")
-        .setDescription("/do Un celador llevaría al paciente hacia el BOX correspondiente. (Aquel donde acude el médico) \n \n /do Le realizarian toma de tensión, oxigeno en sangre y pulsaciones al paciente. \n \n /do ¿El paciente tendría algún parametro fuera de lo común? Si es así, ¿Cual?")
-        .setColor(0xF8281B) 
+        .setTitle("Guía médica")
+        .setDescription("¡Aquí puedes encontrar más detalles de la guía médica que iremos actualizando y llenando! https://cutt.ly/ll1HtWd")
+        .setColor(0xFFFFFF) 
     message.channel.send({embed});
 
 };
--- commands/informacion/textosic3.js
+++ commands/informacion/textosic3.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Procedimiento - Alta de Hospital")
-        .setDescription("/do Llega al box, un celador junto a varios papeles donde incluirian detalles sobre el ingreso del paciente. \n \n /do En estos, aparecerían datos del paciente y actos realizados en el hospital. Debajo del todo, una firma del médico y LSMA. \n \n /do Le harian entrega de los respectivos documentos al paciente. \n \n **(En caso de no rolear el acompañar al sujeto hacia la recepción)** /do Tras esto, le acompañarian hacia la recepción mientras se observa a varias personas aplaudir por la recuperación. ")
+        .setTitle("Procedimiento - Retirada de sangre")
+        .setDescription("/me coge una cinta y posteriormente procede a su colocación en la zona del antebrazo observando al paciente. \n \n /do ¿Las venas serían facilmente reconocibles? \n \n /me coge una jeringa y procede a destapar el capuchón tras esto la prepara. \n \n /me procede con la retirada de sangre, yendo poco a poco y observando al paciente. \n \n /do ¿Se resistira? ¿Cual sería su reacción? \n \n /me una vez extraida la sangre, procede a retirar la jeringa y lo mete en un pequeño bote. \n \n **(Una vez realizado deberíamos llevarlo al laboratorio para que sea examinada)** \n \n /do Se escucharía sonidos provenientes del laboratorio. Beep, boop. Beep, boop. \n \n /do Tras unos 15 minutos los resultados estarían listos.")
         .setColor(0xF8281B) 
     message.channel.send({embed});
 
