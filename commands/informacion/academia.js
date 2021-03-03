c6d4a1eebb783c595f6fa8c9800bb3f095c40e63
Glitch (lsma-project) <none>
2021-03-03 16:25:10 +0000

    ❄️🌈 Checkpoint ./commands/informacion/academia.js:21439783/1015

--- commands/informacion/academia.js
+++ commands/informacion/academia.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Uniforme médico reglamentario")
-        .setDescription("¡Este es el uniforme que debes portar si estas en el hospital!")
+        .setDescription("¡Este es el uniforme que debes portar si estas en el hospital! \n \n **Lista de la compra:** \n \n Tops 249 (0) - Tienda Surburban \n Camiseta 96 (0) - Tienda Mission Row \n Pantalones 129 (0) - Tienda Mission Row \n Torso 90 (0) - Cualquier tienda \n Zapatos 77 (4) - Tienda Mission Row \n \n **Pedir adicionalmente este /ropat:** \n /reportar Hola, necesito que me coloqueis el siguiente ropat: /ropat 7 126 0 ID \n \n *Los cambios de ropa se deberán hacer en la oficina de central antes de acudir al hospital. Recuerda que puedes guardar el conjunto para ponertelo de forma más rapida.*")
         .setColor(0xFFFFFF) 
         .setImage('https://i.imgur.com/Phin9KK.png')
     message.channel.send({embed});
