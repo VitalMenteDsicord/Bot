a7af1b25eb7b6ae1bcf1a4cb82b8ad02c6fb1c55
Glitch (lsma-project) <none>
2021-03-26 17:57:01 +0000

    🗃🌄 Checkpoint ./commands/informacion/normativa.js:30188798/348

--- commands/informacion/normativa.js
+++ commands/informacion/normativa.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message) => {
     let embed = new Discord.MessageEmbed()
 
         .setTitle("Autorizados por LSMA®")
-        .setDescription("¡Hola! Se pone a disposición, esta lista de nuestros miembros del departamento médico autorizados a permanecer en el hospital y actuar como tal. \n \n \n **Director General** Jhon Wood \n \n **Director dept. Médico** Marc Pinto \n \n **Director dept. Urgencias** Ignacio Rueda \n \n **Director dept. Salud Mental** Venancio Gutierrez \n \n **Enfermero** Marc Pinto \n \n **Enfermero** Nicholas Thompson  \n \n **Enfermero** Rafael Kudo \n \n **Enfermero** Vilet Adams \n \n **Celador** Arturo Fernandez \n \n **Celador** Luke Brow \n \n **Celador** Mauro Suárez \n \n **Enfermero** Nicholas Thompson \n \n **Auxiliar de enfermeria** Ethan Weller \n \n **Celador** Francisco Auditore")
+        .setDescription("¡Hola! Se pone a disposición, esta lista de nuestros miembros del departamento médico de la LSMA® autorizados a permanecer en el hospital y actuar como tal. \n \n \n **Director General** Jhon Wood \n \n **Director dept. Médico** Marc Pinto \n \n **Director dept. Urgencias** Ignacio Rueda \n \n **Director dept. Salud Mental** Venancio Gutierrez \n \n **Enfermero** Marc Pinto \n \n **Enfermero** Nicholas Thompson  \n \n **Enfermero** Rafael Kudo \n \n **Enfermero** Vilet Adams  \n \n **Enfermero** Nicholas Thompson \n \n **Auxiliar de enfermeria** Ethan Weller \n \n **Celador** Arturo Fernandez \n \n **Celador** Luke Brow \n \n **Celador** Mauro Suárez \n \n **Celador** Francisco Auditore ")
         .setColor(0xF8281B) 
         .setImage('https://i.imgur.com/CgXE8Vy.png')
         .setFooter('Pillbox® Data Center ', 'https://i.imgur.com/lCJUNKB.png')
