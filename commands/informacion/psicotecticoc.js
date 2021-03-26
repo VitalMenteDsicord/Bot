446d59798e32d95b2326249da440ddb5197dcd2b
Glitch (lsma-project) <none>
2021-03-26 19:30:18 +0000

    🕶🚙 Checkpoint ./commands/informacion/psicotecticoc.js:30188798/372

--- commands/informacion/psicotecticoc.js
+++ commands/informacion/psicotecticoc.js
@@ -4,8 +4,9 @@ module.exports.run = async (bot, message) => {
   
     let embed = new Discord.MessageEmbed()
 
-        .setTitle("Psicoctetico")
-        .setDescription("Buenas gracias por confiar en nosotros \n Necesitamos que rellene esta po
+        .setTitle("Psicoctenco")
+        .setDescription("  Buenas gracias por confiar en nosotros \n Necesitamos que rellene esta plantilla:\n -Nombre: \n 
+                        y hora  que deseas hacerte el psicoctenico  \n Gracias \n Att. LSMA® ")
         .setColor(0x037FDA) 
     message.channel.send({embed});
 
@@ -13,5 +14,5 @@ module.exports.run = async (bot, message) => {
 
 
 module.exports.help = {
-    name: "sugerencia2" 
+    name: "psicoctenico" 
 };
